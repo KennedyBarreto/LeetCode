69b54a1b61458f68b5afb21e1c1685f360daf210
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-01-14 13:15:49 -0300

    feat: solved exercise 1

---
+++ javascript/1-two-sum.js
@@ -0,0 +1,9 @@
+const numIndices = {};
+
+for (let i = 0; i < nums.length; i++) {
+  const complement = target - nums[i];
+  if (numIndices.hasOwnProperty(complement)) {
+    return [numIndices[complement], i];
+  }
+  numIndices[nums[i]] = i;
+}
