173db45cd115493e5af14f14ce420158a4384bc6
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-01-16 17:32:32 -0300

    feat: solved exercise 66

--- javascript/66-plus-one.js
+++ javascript/66-plus-one.js
@@ -3,11 +3,18 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  if (digits[digits.length - 1] == 9) {
-    digits[digits.length - 1] = 1;
-    digits.push(0);
-    return digits;
+  let carry = 1;
+  const result = [];
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let sum = digits[i] + carry;
+    result.push(sum % 10);
+    carry = Math.floor(sum / 10);
   }
-  digits[digits.length - 1]++;
-  return digits;
+
+  if (carry > 0) {
+    result.push(carry);
+  }
+
+  return result.reverse();
 };
